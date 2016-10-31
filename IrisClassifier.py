a65723d377a380d4a398834ce312c64abd174bbe
Matiur Rahman Minar <minar09.bd@gmail.com>
2016-10-31 23:37:00 +0600

    tidying and testing data!

--- IrisClassifier.py
+++ IrisClassifier.py
@@ -14,4 +14,46 @@ iris_data.loc[iris_data['class'] == 'Iris-setossa', 'class'] = 'Iris-setosa'
 
 iris_data['class'].unique()
 
+iris_data = iris_data.loc[(iris_data['class'] != 'Iris-setosa') | (iris_data['sepal_width_cm'] >= 2.5)]
+iris_data.loc[iris_data['class'] == 'Iris-setosa', 'sepal_width_cm'].hist()
+
+iris_data.loc[(iris_data['class'] == 'Iris-versicolor') &
+              (iris_data['sepal_length_cm'] < 1.0),
+              'sepal_length_cm'] *= 100.0
+
+iris_data.loc[iris_data['class'] == 'Iris-versicolor', 'sepal_length_cm'].hist()
+
+iris_data.loc[(iris_data['sepal_length_cm'].isnull()) |
+              (iris_data['sepal_width_cm'].isnull()) |
+              (iris_data['petal_length_cm'].isnull()) |
+              (iris_data['petal_width_cm'].isnull())]
+              
+iris_data.loc[iris_data['class'] == 'Iris-setosa', 'petal_width_cm'].hist()
+
+average_petal_width = iris_data.loc[iris_data['class'] == 'Iris-setosa', 'petal_width_cm'].mean()
+
+iris_data.loc[(iris_data['class'] == 'Iris-setosa') &
+              (iris_data['petal_width_cm'].isnull()),
+              'petal_width_cm'] = average_petal_width
+
+iris_data.loc[(iris_data['class'] == 'Iris-setosa') &
+              (iris_data['petal_width_cm'] == average_petal_width)]
+              
+iris_data.loc[(iris_data['sepal_length_cm'].isnull()) |
+              (iris_data['sepal_width_cm'].isnull()) |
+              (iris_data['petal_length_cm'].isnull()) |
+              (iris_data['petal_width_cm'].isnull())]
+              
+iris_data.to_csv('iris-data-clean.csv', index=False)
+
+iris_data_clean = pd.read_csv('iris-data-clean.csv')
+
 sb.pairplot(iris_data.dropna(), hue='class')
+sb.pairplot(iris_data_clean, hue='class')
+
+assert len(iris_data_clean['class'].unique()) == 3
+assert iris_data_clean.loc[iris_data_clean['class'] == 'Iris-versicolor', 'sepal_length_cm'].min() >= 2.5
+assert len(iris_data_clean.loc[(iris_data_clean['sepal_length_cm'].isnull()) |
+                               (iris_data_clean['sepal_width_cm'].isnull()) |
+                               (iris_data_clean['petal_length_cm'].isnull()) |
+                               (iris_data_clean['petal_width_cm'].isnull())]) == 0
